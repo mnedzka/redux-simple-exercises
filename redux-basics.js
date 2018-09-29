e34a779e587b8513981b97382797efe707cbcb2e
Michal Nedzka <michael.nedzka@gmail.com>
2018-09-29 16:58:19 +0200

    Connect in index.js

--- redux-basics.js
+++ redux-basics.js
@@ -29,3 +29,8 @@ console.log(store.getState());
 store.dispatch({ type: "INC_COUNTER" });
 store.dispatch({ type: "ADD_COUNTER", value: 10 });
 console.log(store.getState());
+
+// Subscription
+store.subscribe(() => {
+  console.log("[Subscription]", store.getState());
+});
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import rootReducer from "./store/reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
