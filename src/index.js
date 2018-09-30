b1da7028c2fb64460f8431e81def5417cb80233a
Michal Nedzka <michael.nedzka@gmail.com>
2018-09-30 19:53:35 +0200

    Add react-redux package, connect main app to provider, add mapStateToProps and mapDispatchToProps in counter component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
@@ -8,5 +9,10 @@ import rootReducer from "./store/reducers";
 
 const store = createStore(rootReducer);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
