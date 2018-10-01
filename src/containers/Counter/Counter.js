dccc31b0e88b1711a0072026f5050a9033d7a857
Michal Nedzka <michael.nedzka@gmail.com>
2018-10-01 22:05:03 +0200

    Add actions in variables instead of strings

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import * as actionTypes from "../../store/actions";
 
 import CounterControl from "../../components/CounterControl/CounterControl";
 import CounterOutput from "../../components/CounterOutput/CounterOutput";
@@ -50,12 +51,14 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
-    onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
-    onAddCounter: () => dispatch({ type: "ADD", payload: 10 }),
-    onSubtractCounter: () => dispatch({ type: "SUBTRACT", payload: -10 }),
-    onStoreResult: () => dispatch({ type: "STORE_RESULT" }),
-    onDeleteResult: id => dispatch({ type: "DELETE_RESULT", toDelete: id })
+    onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
+    onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
+    onAddCounter: () => dispatch({ type: actionTypes.ADD, payload: 10 }),
+    onSubtractCounter: () =>
+      dispatch({ type: actionTypes.SUBTRACT, payload: -10 }),
+    onStoreResult: () => dispatch({ type: actionTypes.STORE_RESULT }),
+    onDeleteResult: id =>
+      dispatch({ type: actionTypes.DELETE_RESULT, toDelete: id })
   };
 };
 
